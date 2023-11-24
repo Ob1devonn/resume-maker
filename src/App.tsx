dcdecd9fa3a517f35b632fa97caaafc363fd61a2
DeVonn <ob1devonn@gmail.com>
2023-11-24 09:57:07 -0500

    Create Resume and states for it

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,60 @@
+import { useState } from "react";
+import Resume from "./components/Resume";
+// personal: {
+//   name: string;
+//   email: string;
+//   number: number;
+//   address: string;
+// };
+// educational: {
+//   schoolName: string;
+//   study: string;
+//   date: string;
+//   details: string;
+// };
+// occupational: {
+//   company: string;
+//   title: string;
+//   date: string;
+//   details: string;
+// };
 const App = () => {
+  const [personal, setPersonal] = useState({
+    name: "John Doe",
+    email: "johndoe@gmail.com",
+    number: 1234567891,
+    address: "123 Main st. Denver, Co. 80222",
+  });
+  const [educational, setEducational] = useState({
+    schoolName: "Stuff",
+    study: "Stuff",
+    date: "Stuff",
+    details: "Stuff",
+  });
+
+  const [occupational, setOccupational] = useState({
+    company: "CorpCo",
+    title: "Lead",
+    date: "3/10/20",
+    details:
+      "Did some stuff, along with other stuff, that was required for the job",
+  });
+
   return (
     <div className="container">
       <main>
+        <Resume
+          personal={personal}
+          educational={educational}
+          occupational={occupational}
+        />
+      </main>
+      <aside>
         <form className="">
           <label htmlFor="name">Name</label>
           <input type="text" id="name" />
         </form>
-      </main>
-      <aside></aside>
+      </aside>
       <footer></footer>
     </div>
   );
---
+++ src/components/Resume.tsx
@@ -0,0 +1,124 @@
+interface Props {
+  personal: {
+    name: string;
+    email: string;
+    number: number;
+    address: string;
+  };
+  educational: {
+    schoolName: string;
+    study: string;
+    date: string;
+    details: string;
+  };
+  occupational: {
+    company: string;
+    title: string;
+    date: string;
+    details: string;
+  };
+  //  education: [];
+  //experience: [];
+}
+/*, education, experience */
+export default function Resume({ personal, educational, occupational }: Props) {
+  const name = personal.name ? <h2>{personal.name}</h2> : <h2>name</h2>;
+  const email = personal.email ? (
+    <p className="contact-combo">{personal.email}</p>
+  ) : (
+    <p className="contact-combo">Email</p>
+  );
+
+  const number = personal.number ? (
+    <p className="contact-combo">{personal.number}</p>
+  ) : (
+    <p className="contact-combo">number</p>
+  );
+
+  const address = personal.address ? (
+    <p className="contact-combo">{personal.address}</p>
+  ) : (
+    <p className="contact-combo">Address</p>
+  );
+
+  const schoolDate = educational.date ? (
+    <time>{educational.date}</time>
+  ) : (
+    <time dateTime="2000-01-01">January 1, 2000</time>
+  );
+  const schoolName = educational.schoolName ? (
+    <h4>{educational.schoolName}</h4>
+  ) : (
+    <h4>SchoolName</h4>
+  );
+  const degree = educational.study ? <p>{educational.study}</p> : <p>Degree</p>;
+  const schoolDetails = educational.details ? (
+    <p>{educational.details}</p>
+  ) : (
+    <p>
+      Lorem ipsum dolor sit amet consectetur adipisicing elit. Architecto
+      dolorum omnis quidem voluptatem esse error, optio ea vitae tempore
+      dolorem.
+    </p>
+  );
+  const jobDate = occupational.date ? (
+    <time>{occupational.date}</time>
+  ) : (
+    <time dateTime="2000-01-01">January 1, 2000</time>
+  );
+
+  const company = occupational.company ? (
+    <h4>{occupational.company}</h4>
+  ) : (
+    <h4>Company</h4>
+  );
+
+  const title = occupational.title ? <p>{occupational.title}</p> : <p>Title</p>;
+
+  const jobDetails = occupational.details ? (
+    <p>{occupational.details}</p>
+  ) : (
+    <p>
+      Lorem ipsum dolor sit amet consectetur, adipisicing elit. Tenetur qui
+      atque quae corrupti at ullam culpa soluta repudiandae, autem blanditiis!
+    </p>
+  );
+  return (
+    <div className="resume">
+      <div className="header">
+        {name}
+        <div className="contact">
+          {email}
+          {number}
+          {address}
+        </div>
+      </div>
+
+      <div className="body-sec">
+        <div className="sections">
+          <h3>Education</h3>
+          <div className="details">
+            {schoolDate}
+            <div>
+              {schoolName}
+              {degree}
+            </div>
+            {schoolDetails}
+          </div>
+        </div>
+        <div className="sections">
+          <h3>Experience</h3>
+
+          <div className="details">
+            {jobDate}
+            <div className="title-subtitle">
+              {company}
+              {title}
+            </div>
+            {jobDetails}
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+}
